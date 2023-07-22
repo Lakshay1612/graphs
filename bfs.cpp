d58a3952483b41523b19edf2c0901bbdd329cd98
lakshay jain <lakshay1612@gmail.com>
2023-07-22 15:30:29 +0530

    second commit

---
+++ bfs.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+void prepareAdjList(unordered_map<int, set<int>> &adjList, vector<pair<int, int>> &edges)
+{
+
+    for (auto i : edges)
+    {
+        auto u = i.first;
+        auto v = i.second;
+        adjList[v].insert(u);
+        adjList[u].insert(v);
+    }
+}
+
+void bfs(unordered_map<int, set<int>> &adjList, unordered_map<int, bool> &visited,
+         vector<int> &ans, int node)
+{
+    queue<int> q;
+    q.push(node);
+    visited[node] = 1;
+
+    while (!q.empty())
+    {
+        int frontNode = q.front();
+        q.pop();
+
+        ans.push_back(frontNode);
+        for (auto i : adjList[frontNode])
+        {
+            if (!visited[i])
+            {
+                q.push(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+vector<int> BFS(int vertex, vector<pair<int, int>> edges)
+{
+    // Write your code here
+    unordered_map<int, set<int>> adjList;
+    unordered_map<int, bool> visited;
+    vector<int> ans;
+    prepareAdjList(adjList, edges);
+
+    for (int i = 0; i < vertex; i++)
+    {
+        if (!visited[i])
+        {
+            bfs(adjList, visited, ans, i);
+        }
+    }
+
+    return ans;
+}
